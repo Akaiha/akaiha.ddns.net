f9c582522a18320400aba8cd3a311bae3661de81
Aera <aeragamint6@gmail.com>
2017-03-21 20:33:42 +0100

    Animations sur les sections headers Animations sur les centres d'intérêts Modifs CSS pour les liens

--- ressources/script.js
+++ ressources/script.js
@@ -1,28 +1,59 @@
-// Add scrollspy to <body>
-$('body').scrollspy({target: ".navbar", offset: 60});
+$(document).ready(function(){
 
-// Add smooth scrolling on all links inside the navbar
-$("#myNavbar a").on('click', function(event) {
+  // Add scrollspy to <body>
+  $('body').scrollspy({target: ".navbar", offset: 60});
 
-  // Make sure this.hash has a value before overriding default behavior
-  if (this.hash !== "") {
+  // Add smooth scrolling on all links inside the navbar
+  $("#myNavbar a").on('click', function(event) {
 
-    // Prevent default anchor click behavior
-    event.preventDefault();
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
 
-    // Store hash
-    var hash = this.hash;
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
-    // Using jQuery's animate() method to add smooth page scroll
-    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 600, function(){
+      // Store hash
+      var hash = this.hash;
 
-    // Add hash (#) to URL when done scrolling (default click behavior)
-      window.location.hash = hash;
-    });
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 600, function(){
 
-  } // End if
+      // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+
+    } // End if
+
+  });
+
+  //animate interests on hover (zoom-in like)
+  $(".interests p").hover( 
+    function() {
+      $(this).animate({fontSize: '1.1em'}, "200");
+    }, 
+    function() {
+      $(this).animate({fontSize: '1em'}, "200");
+  });
+
+  //Animate section titles on hover (slide left-right)
+  $(".page-header .align-left").hover(
+    function(){
+      $(this).animate({left : "25px"},'slow');
+    },
+    function(){
+      $(this).animate({left : "-=25px"}, 'slow');
+  });
+
+  //Animate section titles on hover (slide right-left)
+  $(".page-header .align-right").hover(
+    function(){
+      $(this).animate({left : "-=25px"},'slow');
+    },
+    function(){
+      $(this).animate({left : "+=25px"}, 'slow');
+  });
 
 });
